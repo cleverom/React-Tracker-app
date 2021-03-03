11ae60ef007b785c3517bf28f204996d0aba489c
cleverom <clevereziogor@gmail.com>
2021-03-03 05:45:20 +0100

    set up react application

---
+++ src/components/button.js
@@ -0,0 +1,21 @@
+import PropTypes from 'prop-types'
+
+
+
+const button = ({ color, text, onClick }) => {
+
+    return <button onClick={onClick} style={{backgroundColor: color }} className="btn">{ text }</button>
+}
+
+
+button.defaultProps = {
+    color: 'steelblue'
+}
+
+button.propTypes = {
+    text: PropTypes.string,
+    color: PropTypes.string,
+    onCick: PropTypes.func,
+}
+
+export default button
